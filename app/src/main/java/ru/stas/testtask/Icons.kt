a6b1a958732f5a0eb4c664574ea7418d2d14947f
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2023-02-28 15:42:46 +0300

    start page one continue

---
+++ app/src/main/java/ru/stas/testtask/Icons.kt
@@ -0,0 +1,10 @@
+package ru.stas.testtask
+
+enum class Icon(val drawableId: Int) {
+    Phones(R.drawable.phone),
+    Headphones(R.drawable.headphones),
+    Games(R.drawable.games),
+    Cars(R.drawable.cars),
+    Furniture(R.drawable.furnitura),
+    Kids(R.drawable.kids),
+}
