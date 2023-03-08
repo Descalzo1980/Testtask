87c17c73b75d8d2058a172fe7a985f6fd4b88e6a
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2023-03-08 22:02:19 +0300

    fix bottom navigation

--- app/src/main/java/ru/stas/testtask/SingInFragment.kt
+++ app/src/main/java/ru/stas/testtask/SingInFragment.kt
@@ -1,13 +1,11 @@
 package ru.stas.testtask
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextUtils
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
