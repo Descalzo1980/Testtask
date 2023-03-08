7aaf4d5e6e60485b1ffa000e2a1bfa1fc1ea4ce2
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2023-03-08 19:56:51 +0300

    fix bottom navigation

--- app/src/main/java/ru/stas/testtask/MainActivity.kt
+++ app/src/main/java/ru/stas/testtask/MainActivity.kt
@@ -1,19 +1,54 @@
 package ru.stas.testtask
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.AppBarConfiguration
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import ru.stas.testtask.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var navController: NavController
+    private lateinit var navHostFragment: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.bottomNavigation.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.profileFragment, R.id.pageOneFragment, R.id.pageTwoFragment
+                -> {
+                    binding.bottomNavigation.visibility = View.VISIBLE
+                }
+                else -> {
+                    binding.bottomNavigation.visibility = View.GONE
+                }
+            }
+        }
+        NavigationUI.setupWithNavController(binding.bottomNavigation, navController)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.singInFragment -> {
+                navHostFragment.navController
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
